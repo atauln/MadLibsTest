eaede93d85f3db71a36ad53a1b89df97b0bb5103
Ataul <ataulnoor75@gmail.com>
2018-11-12 11:24:51 -0500

    Add project files.

---
+++ Module1.vb
@@ -0,0 +1,25 @@
+﻿Module Module1
+    Sub Lvl1(name)
+        Console.Clear()
+        Console.WriteLine("Welcome to Level 1, " + name)
+        Console.ReadKey(True)
+    End Sub
+    Sub Main()
+        Console.WriteLine("Welcome to Mad Libs Lite!")
+        Console.WriteLine("What is your name?")
+        Dim NameofUser = Console.ReadLine()
+        Console.WriteLine("Thanks, " + NameofUser)
+        Console.WriteLine("What level would you like to play?")
+        Dim numLvlResponse = Console.ReadLine()
+        Console.WriteLine("Transporting " + NameofUser + " to  level " + numLvlResponse + "...")
+        If numLvlResponse = 1 Then
+            Lvl1(NameofUser)
+        Else
+            Console.WriteLine("That is not a valid response...")
+            Console.WriteLine("Terminating program...")
+            Console.ReadKey(True)
+            End
+        End If
+    End Sub
+
+End Module
