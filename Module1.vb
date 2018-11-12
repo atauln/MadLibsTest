5c591ef2b70b5584140261833cea2360634b4abf
Ataul <ataulnoor75@gmail.com>
2018-11-12 12:01:13 -0500

    raNDOMIZE LVL1

--- Module1.vb
+++ Module1.vb
@@ -1,11 +1,31 @@
 ﻿Module Module1
+    Dim Lvl1Strings2 = {"creeper", "zombie", "husk", "skeleton", "wither"}
+    Dim Lvl1Strings1 = {"princess", "bed", "bedroom", "XBox", "footon"}
     Sub Press()
         Console.WriteLine("Press any key..")
+        Console.ReadKey(True)
     End Sub
     Sub Lvl1(name)
         Console.Clear()
         Console.WriteLine("Welcome to Level 1, " + name)
-        Console.ReadKey(True)
+        Press()
+        Console.WriteLine("Adjective: ")
+        Dim Adj1 = Console.ReadLine()
+        Console.WriteLine("Verb: ")
+        Dim Vrb1 = Console.ReadLine()
+        Console.WriteLine("Your story is done!")
+        Press()
+        Randomize()
+        Dim Slcted1 = Math.Round(Rnd() * 5) - 1
+        Dim Slcted2 = Math.Round(Rnd() * 5) - 1
+        Console.WriteLine("The " + Adj1 + " " + Lvl1Strings1(Slcted1) + " was " + Vrb1 + "ing away the " + Lvl1Strings2(Slcted2) + ".")
+        Press()
+        End
+    End Sub
+    Sub Lvl2(name)
+        Console.Clear()
+        Console.WriteLine("Welcome to Level 2, " + name)
+        Press()
     End Sub
     Sub Main()
         Console.WriteLine("Welcome to Mad Libs Lite!")
@@ -17,10 +37,12 @@
         Console.WriteLine("Transporting " + NameofUser + " to  level " + numLvlResponse + "...")
         If numLvlResponse = 1 Then
             Lvl1(NameofUser)
+        ElseIf numLvlResponse = 2 Then
+            Lvl2(NameofUser)
         Else
             Console.WriteLine("That is not a valid response...")
             Console.WriteLine("Terminating program...")
-            Console.ReadKey(True)
+            Press()
             End
         End If
     End Sub
